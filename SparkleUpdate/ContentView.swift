118c66bf058f378c381b0f117a87202928ade73b
Kazuho Okui <63257+naan@users.noreply.github.com>
2022-08-09 10:28:05 -0400

    Initial Commit

---
+++ SparkleUpdate/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SparkleUpdate
+//
+//  Created by Kazuho Okui on 8/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+		Text("Hello world")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
